ef89daf873819dea009c87bf03ad852a9f026749
Wanjiru393 <carolciru393@gmail.com>
2022-09-21 20:01:15 +0300

    created Models

--- fliq/admin.py
+++ fliq/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Item, Order, OrderItem
 
-# Register your models here.
+#Define item display
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+    list_display = [
+        'title',
+        'price',
+        'discount_price',
+        'description'
+    ]
+
+#Register models
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
