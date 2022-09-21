af412ca12f900caba4262da3251ece4680e565d2
Wanjiru393 <carolciru393@gmail.com>
2022-09-21 20:50:36 +0300

    Added class-based views

--- fliq/views.py
+++ fliq/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 
+#Create Class-based views
+from django.views.generic import ListView,DetailView
+from .models import Item, Order, OrderItem
+
+class HomeView(ListView):
+    model = Item
+    template_name = 'home.html'
+
+
 # Create your views here.
 def home(request):
-    context = {}
-    return render(request, 'store/home.html' ,context)
+    return render(request, 'store/home.html')
 
 def cart(request):
-    context = {}
-    return render(request, 'store/cart.html' ,context)
+    return render(request, 'store/cart.html')
 
 def checkout(request):
-    context = {}
-    return render(request, 'store/checkout.html' ,context)
+    return render(request, 'store/checkout.html')
