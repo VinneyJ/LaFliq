30e8b7fedef1e8541ded388903d06da8b3e6d33d
Wanjiru393 <carolciru393@gmail.com>
2022-10-05 21:23:46 +0300

    Removed Cart view

--- fliq/views.py
+++ fliq/views.py
@@ -8,12 +8,6 @@ def home(request):
     return render(request, 'store/home.html', context)
 
 def cart(request):
-    if request.user.is_authenticated:
-        user = request.user.customer
-        order, created = Order.objects.get_or_create(username=user)
-        items = order.orderitem_set.all()
-    else:
-        items = []
     context = {'items':items} 
     return render(request, 'store/cart.html', context)
 
